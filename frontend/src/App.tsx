6b959b64bdfdd81bafbd0d440c1e99586f25eeea
Emanuel Nunes <emanuelsoares02@gmail.com>
2026-02-23 23:36:39 -0300

    fix: restrict dev shortcuts to development builds only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -218,7 +218,7 @@ function App() {
       <BrowserRouter
         future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
       >
-        <DevShortcuts />
+        {import.meta.env.DEV && <DevShortcuts />}
         <OnboardingModal />
         <UpdateModal />
         <Suspense
